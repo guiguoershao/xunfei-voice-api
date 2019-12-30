60639931108b8e06e567a0f0ff6402c1f52832a9
fengyan <guiguoershao@163.com>
2019-12-30 17:08:47 +0800

    语音合成接口

--- tests/test.php
+++ tests/test.php
@@ -1,5 +1,10 @@
 <?php
 
+$config = [
+    'appId' => '5d52239f',
+    'apiKey' => '8c03514c1951b0a7a67faad578cffb0d',
+    'apiSecret' => '75c1fa573d29b82e3fa6abb4512e8004',
+];
 $tts = new \Guiguoershao\XunFeiVoiceApi\Services\Tts\OnlineTtsService();
 $result = $tts->request('你好世界');
 var_dump($result);
